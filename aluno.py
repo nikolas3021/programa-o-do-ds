4d510693efe3f90ec8ef8423a47b8b4a79e7a7ef
nikolas3021 <nokolascristiandasilva2007@gmail.com>
2025-05-27 08:29:09 -0300

    Update aluno.py

--- aluno.py
+++ aluno.py
@@ -1,25 +1,10 @@
-import functools
+from dataclasses import dataclass
 
+@dataclass
 class Aluno:
-
-    def __init__(self, nome, nota):
-
-        self.nome = nome
-
-        self.nota = nota
-
-    def __repr__(self):
-
-        return f'{self.nome}: {self.nota}'
-
-
-
-def comparar_alunos(a, b):
-
-    return (a.nota > b.nota) - (a.nota < b.nota)
+    nome: str
+    nota: int
 
 alunos = [Aluno('Ana', 88), Aluno('Rafael', 92), Aluno('Paulo', 78)]
 
-alunos_ordenados = sorted(alunos, key=functools.cmp_to_key(comparar_alunos))
-
-print(alunos_ordenados)
+print(sorted(alunos, key=lambda a: -a.nota))
